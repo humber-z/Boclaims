710a3f88b4b9f0256f0f649fc340c7cd1b964d77
humber-z <jhmoralesn@live.com.mx>
2019-10-18 16:13:08 -0700

    1 file modified

--- Components/CaseManager/editor/Fases/Fase1.js
+++ Components/CaseManager/editor/Fases/Fase1.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {
   MuiPickersUtilsProvider,
-  KeyboardDatePicker,
+  KeyboardDatePicker, 
 } from '@material-ui/pickers';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
